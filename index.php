bdb42ad03ec491491edb7ba2692d1c7405a94c97
Ekampreet Kaur <ekaur@mail.greenriver.edu>
2020-01-06 14:08:43 -0500

    Created index file

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/** created by phpstorm... */
+?>
+<!DOCTYPE html>
+<body lang="en">
+<head>
+<body>
+<meta charset="UTF-8">
+<title><?php echo $title;  ?></title>
+<h2>Hi</h2>
+<h3>Hello!</h3>
+
